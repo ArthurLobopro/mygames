7890520b8e253c67e3a8b2c6caf0aeebd5e4621b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-17 23:11:00 -0300

    index att, fonte adicionada

--- jogo-da-velha/index.js
+++ jogo-da-velha/index.js
@@ -1,14 +1,40 @@
-function draw_x(id) {
-    const canvas = document.getElementById(id)
-    const ctx = canvas.getContext('2d')
-    const x = new Image()
-    x.src='midia/x.png'
-    ctx.drawImage(x, 10 , 10)
+const game = {
+    o: 'midia/o.png',
+    x:'midia/x.png',
+    status: [
+        '','','',
+        '','','',
+        '','',''
+    ],
 }
-function draw_o(id) {
-    const canvas = document.getElementById(id)
-    const ctx = canvas.getContext('2d')
-    const o = new Image()
-    o.src='midia/o.png'
-    ctx.drawImage(o, 8, 7)
+function jogada(player) {
+    if(game.status[player]==''){
+        game.status.player='x'
+        draw(3,game.x)
+        setTimeout(bot(), 3000);
+    }else{
+        alert('Jogada inválida')
+    }
+}
+function bot() {
+    function randint(min,max){
+        return Math.floor(Math.random() * (max-min+1)) + min
+    }
+    let bot = randint(0,9)
+    if(game.status[bot]==''){
+        game.status[bot]='o'
+        draw(bot,game.o)
+    }else{
+        bot()
+    }
+}
+function draw(id,src) {
+    const div = document.getElementById(id)
+    const o = `<img src='${src}' class='show'>`
+    div.innerHTML=o
+}
+function zera_status(){
+    for(i in game.status){
+        game.status=''
+    }
 }
