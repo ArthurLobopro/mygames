4447dd66cc98c2386496bf56132fa6d7e244a1ee
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-18 12:24:16 -0300

    Algumas mudanças, jogo quase pronto

--- jogo-da-velha/index.js
+++ jogo-da-velha/index.js
@@ -1,40 +1,71 @@
 const game = {
-    o: 'midia/o.png',
-    x:'midia/x.png',
     status: [
         '','','',
         '','','',
         '','',''
     ],
+    stsClass: [
+
+    ],
 }
-function jogada(player) {
-    if(game.status[player]==''){
-        game.status.player='x'
-        draw(3,game.x)
-        setTimeout(bot(), 3000);
+function jogada(player,alerta=true) {
+    if(game.status[player-1]==''){
+        game.status[player-1]='x'
+        draw(player,'x')
+        let disp = false
+        for(i of game.status){
+            if(i==''){
+                disp=true
+                break
+            }
+        }
+        if(disp==true){
+            bot()
+            verifica()
+        }else{
+            verifica()
+        }
+        
+        console.log(game.status)
     }else{
-        alert('Jogada inválida')
+        (alerta==true) ? alert('Jogada inválida') : null
     }
 }
 function bot() {
-    function randint(min,max){
-        return Math.floor(Math.random() * (max-min+1)) + min
-    }
-    let bot = randint(0,9)
-    if(game.status[bot]==''){
-        game.status[bot]='o'
-        draw(bot,game.o)
+    const randint = (min,max) => Math.floor(Math.random() * (max-min+1)) + min
+    let jbot = randint(1,9)
+    console.log(jbot)
+    if(game.status[jbot-1]==''){
+        game.status[jbot-1]='o'
+        draw(jbot,'o')
     }else{
         bot()
     }
 }
 function draw(id,src) {
-    const div = document.getElementById(id)
-    const o = `<img src='${src}' class='show'>`
-    div.innerHTML=o
+    document.getElementById(`${id}`).innerHTML=`<img src='midia/${src}.png' class='show${src}'>`
+}
+function verifica() {
+    const sts = game.status
+    for(i=0;i<9;i+=3){
+        if(sts[i] == sts[i+1] && sts[i] == sts[i+2] && (sts[i]!='')){
+            console.log('victory1')
+        }
+    }
+    for(i=0;i<3;i++){
+        if(sts[i] == sts[i+3] && sts[i] == sts[i+6] && (sts[i]!='')){
+            console.log('victory2')
+        }
+    }
+    if(sts[0] == sts[4] && sts[0] == sts[8] && (sts[0]!='')){
+        console.log('victory3')
+    }
+    if(sts[2] == sts[4] && sts[2] == sts[6] && (sts[0]!='')){
+        console.log('victory4')
+    }
 }
 function zera_status(){
     for(i in game.status){
-        game.status=''
+        game.status[i]=''
     }
 }
