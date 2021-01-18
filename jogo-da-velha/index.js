dea440ca50748153af38e978fa08ccc5a3a18efc
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-18 18:30:39 -0300

    index update

--- jogo-da-velha/index.js
+++ jogo-da-velha/index.js
@@ -5,6 +5,7 @@ const game = {
         '','',''
     ],
 }
+let jogar = () => jogada(String(event.target.id)[3])
 function jogada(player,alerta=true) {
     if(game.status[player-1]==''){
         game.status[player-1]='x'
@@ -16,14 +17,13 @@ function jogada(player,alerta=true) {
                 break
             }
         }
-        document.getElementById(`div${player}`).removeEventListener('click',function(){alert(this.id)},false)
+        document.getElementById(`div${player}`).removeEventListener('click',jogar,false) 
         if(disp==true){
             bot()
             verifica()
         }else{
             verifica()
         }
-        
         console.log(game.status)
     }else{
         (alerta==true) ? alert('Jogada inválida') : null
@@ -45,21 +45,25 @@ function draw(id,src) {
 }
 function verifica() {
     const sts = game.status
-    for(i=0;i<9;i+=3){
+    for(i=0;i<7;i+=3){
         if(sts[i] == sts[i+1] && sts[i] == sts[i+2] && (sts[i]!='')){
             console.log('victory1')
+            win(sts[i],[i+1,i+2,i+3])
         }
     }
     for(i=0;i<3;i++){
         if(sts[i] == sts[i+3] && sts[i] == sts[i+6] && (sts[i]!='')){
             console.log('victory2')
+            win(sts[i],[i+1,i+4,i+7])
         }
     }
     if(sts[0] == sts[4] && sts[0] == sts[8] && (sts[0]!='')){
         console.log('victory3')
+        win(sts[0],[1,5,9])
     }
-    if(sts[2] == sts[4] && sts[2] == sts[6] && (sts[0]!='')){
+    if(sts[2] == sts[4] && sts[2] == sts[6] && (sts[2]!='')){
         console.log('victory4')
+        win(sts[2],[3,5,7])
     }
 }
 function zera_status(){
@@ -67,9 +71,37 @@ function zera_status(){
         game.status[i]=''
     }
 }
+let elemEvt
 function addEventAll(){
-    for(i=0;i<9;i++){
-        document.getElementById(`div${i+1}`).addEventListener('click',function(){jogada(String(this.id)[3])},false)
+    elemEvt = []
+    for(i=0;i<9;i++) {
+      let elem = document.getElementById(`div${i+1}`)
+      elem.addEventListener('click',jogar,false) 
+      elemEvt.push(elem)
+  }
+}
+
+function removeEventAll(){
+    elemEvt= []
+    for(i=0;i<9;i++) {
+        let elem = document.getElementById(`div${i+1}`)
+        elem.removeEventListener('click',jogar,false) 
+        elemEvt.push(elem)
     }
+}
+function win(winner,sequence){
+    const msg = document.getElementById('msg')
+    if(winner=='x'){
+        for( i of sequence){
+            document.getElementById(`div${i}`).innerHTML=`<img src='midia/red-x.png'>`
+        }
+        msg.innerText='Você venceu!'
+    }else{
+        for( i of sequence){
+            document.getElementById(`div${i}`).innerHTML=`<img src='midia/red-o.png'>`
+        }
+        msg.innerText='Você perdeu!'
+    }
+    removeEventAll()
 }
 addEventAll()
