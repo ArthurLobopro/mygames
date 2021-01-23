55c19b49368785f770daa778cff4d8e3826ea13e
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-23 19:57:02 -0300

    botão de começar de novo adicionado

--- par-impar/index.js
+++ par-impar/index.js
@@ -11,7 +11,7 @@ const game = {
 const circle = document.querySelectorAll('.circle')
 const content = document.getElementById('content')
 const option = (e) => showNumbers(e.target.innerText)
-const setnum = (e) => selectNumbers(e.target.innerText)
+const setnum = (e) => selectNumbers(Number(e.target.innerText))
 circle[0].addEventListener('click',option,false)
 circle[1].addEventListener('click',option,false)
 
@@ -34,7 +34,6 @@ function selectNumbers(num){
     const randint = (min,max) => Math.floor(Math.random() * (max-min+1)) + min
     game.player.number=num
     game.bot.number=randint(0,5)
-    content.innerHTML=`<div class='txt'> </div><div class='txt'> </div><div class='img'> </div> </div> <div class='img'></div><div class='show'></div>`
     content.classList.remove('flex')
     content.classList.add('grid')
     function img(value,index,time) {
@@ -49,27 +48,40 @@ function selectNumbers(num){
             document.querySelectorAll('.txt')[index].classList.add('dur1')
         }, time);
     }
-    const win = () => {
-        document.querySelector('.show').innerHTML='Você venceu!'
-        
+    const win = () => 'Você venceu!'
+    const lose = () => 'Você perdeu!'
+
+    const imgString = (value,index) =>{
+        return `<div class='up dur05'><img src="midia/${value}dedos.png" style='transform: ${(index==0)? '': 'rotateY(180deg) '} rotateZ(90deg) ;'></div>`
     }
-    const lose = () =>{
-        document.querySelector('.show').innerHTML='Você perdeu!'
+    
+    let soma = game.player.number + game.bot.number
+    function verifica(){
+        if(soma % 2 == 0){
+            return(game.player.option == 'Par')? win() : lose()
+        }else{
+            return(game.player.option == 'Ímpar')? win() : lose()
+        }
     }
+    
+    content.innerHTML=`
+    <div class='txt' ></div>
+    <div class='txt' ></div>
+    <div class='img'></div>
+    <div class='img'></div>
+    <div class='show hidden' style='animation-delay: 3s'>${verifica()}</div>
+    <button onclick='newgame()' class='hidden'>Jogar outra vez</button>`
     txt(game.player.option,0,1000)
     txt(game.bot.option,1,2000)
     img(game.player.number,0,2700)
     img(game.bot.number,1,2700)
-    let soma = game.player.number + game.bot.number
-    if(soma % 2 == 0){
-        setTimeout(() => {
-            (game.player.option == 'Par')? win() : lose()
-        }, 3300);
-    }else{
-        setTimeout(() => {
-            (game.player.option == 'Ímpar')? win() : lose()
-        }, 3300);
-    }
+    setTimeout(() => {
+        document.querySelector('.show').classList.remove('hidden')
+    }, 3000);
+    setTimeout(() => {
+        document.querySelector('button').classList.remove('hidden')
+    }, 4000);
 }
-
-
+function newgame(){
+    document.location.reload()
+}
